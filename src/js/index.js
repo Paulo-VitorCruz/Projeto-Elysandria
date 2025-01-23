1c7adcd69af6b21d438326ac626fef3642e43a61
Paulo Vitor Cruz <paulovitorcruz.Dev@hotmail.com>
2025-01-23 12:57:02 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -2,25 +2,25 @@ const botoesCarrossel = document.querySelectorAll(".botao");
 const imagens = document.querySelectorAll(".imagem");
 const informacoes = document.querySelectorAll(".informacoes");
 
-// Passo 2 - Identificar o clique do usuário no botão
+
 botoesCarrossel.forEach((botao, indice) => {
   botao.addEventListener("click", () => {
-    // Passo 3 - Desmarcar o botão selecionado anterior
+    
     desativarBotaoSelecionado();
 
-    // Passo 4 - Marcar o botão clicado como selecionado
+    
     marcarBotaoSelecionado(botao);
 
-    // Passo 5 - Esconder a imagem anteriormente ativa
+   
     esconderImagemAtiva();
 
-    // Passo 6 - Mostrar a imagem correspondente ao botão clicado
+    
     mostrarImagemDeFundo(indice);
 
-    // Passo 7 - Esconder a informação anteriormente ativa
+   
     esconderInformacoesAtivas();
 
-    // Passo 8 - Mostrar a informação correspondente ao botão clicado
+   
     mostrarInformacoes(indice);
   });
 });
